0c273c29b1e1b5251827cecff2921729228f91f9
Michael Kaufman <michaelkauf@gmail.com>
2021-12-12 21:57:40 -0800

    day7, part2

--- day7/simulation.py
+++ day7/simulation.py
@@ -1,34 +1,65 @@
 from file_reader import FileReader
-import pdb
 import sys
+import math
+
+PART_2 = 2
+PART_1 = 1
 
 class Simulation:
-    def __init__(self, crabs):
+    def __init__(self, crabs, version):
         self.best_pos = 0
         self.crabs = crabs
-        self.min_distance_to_dest = sys.maxsize
+        self.min_fuel_usage = sys.maxsize
+        self.version = version
+
+    # nth triangle number formula
+    def calculate_fuel_part_2(self, origin, dest):
+        n = abs(origin - dest)
+        return (math.pow(n, 2) + n) / 2
+
+    def calculate_fuel_part_1(self, origin, dest):
+        return abs(origin - dest) 
+
+    def calculate_fuel(self, origin, dest):
+        if self.version == PART_1:
+            return self.calculate_fuel_part_1(origin, dest) 
+        return self.calculate_fuel_part_2(origin, dest)
 
     def find_best_pos(self):
         # pdb.set_trace()
         min_hor_pos = min(self.crabs)
         max_hor_pos = max(self.crabs)
         for dest in range(min_hor_pos, max_hor_pos + 1):
-            total_distance_to_dest = 0
-            for curr_pos in self.crabs:
-                dist_to_dest = abs(curr_pos - dest) 
-                total_distance_to_dest += dist_to_dest
-                if (total_distance_to_dest > self.min_distance_to_dest):
+            total_fuel_usage = 0
+            for origin in self.crabs:
+                fuel_usage = self.calculate_fuel(origin, dest)
+                total_fuel_usage += fuel_usage
+                if (total_fuel_usage > self.min_fuel_usage):
                     break
 
-            if (total_distance_to_dest < self.min_distance_to_dest):
+            if (total_fuel_usage < self.min_fuel_usage):
                 self.best_pos = dest
-                self.min_distance_to_dest = total_distance_to_dest
+                self.min_fuel_usage = total_fuel_usage
 
-        
+    def find_best_pos_part_1(self):
+        # pdb.set_trace()
+        min_hor_pos = min(self.crabs)
+        max_hor_pos = max(self.crabs)
+        for dest in range(min_hor_pos, max_hor_pos + 1):
+            total_fuel_usage = 0
+            for origin in self.crabs:
+                fuel_usage = abs(origin - dest) 
+                total_fuel_usage += fuel_usage
+                if (total_fuel_usage > self.min_fuel_usage):
+                    break
+
+            if (total_fuel_usage < self.min_fuel_usage):
+                self.best_pos = dest
+                self.min_fuel_usage = total_fuel_usage
 
 if __name__ == "__main__":
     crabs = FileReader('input.txt').process_file()
-    sim = Simulation(crabs)
+    sim = Simulation(crabs, 2)
     sim.find_best_pos()
     print(f'\n-------\nResults\n-------')
-    print(f'best_position: {sim.best_pos}, fuel_usage: {sim.min_distance_to_dest}')
+    print(f'best_position: {sim.best_pos}, fuel_usage: {sim.min_fuel_usage}')
--- day7/tests/test_simulation.py
+++ day7/tests/test_simulation.py
@@ -11,11 +11,21 @@ class SimulationTestCase(unittest.TestCase):
         self.crabs = [16,1,2,0,4,2,7,1,2,14]
 
     def test_find_best_pos(self):
-        sim = Simulation(self.crabs)
+        sim = Simulation(self.crabs, 1)
         sim.find_best_pos()
         self.assertEqual(sim.best_pos, 2)
 
     def test_fuel_usage(self):
-        sim = Simulation(self.crabs)
+        sim = Simulation(self.crabs, 1)
         sim.find_best_pos()
-        self.assertEqual(sim.min_distance_to_dest, 37)
+        self.assertEqual(sim.min_fuel_usage, 37)
+
+    def test_find_best_pos2(self):
+        sim = Simulation(self.crabs, 2)
+        sim.find_best_pos()
+        self.assertEqual(sim.best_pos, 5)
+
+    def test_fuel_usage2(self):
+        sim = Simulation(self.crabs, 2)
+        sim.find_best_pos()
+        self.assertEqual(sim.min_fuel_usage, 168)
