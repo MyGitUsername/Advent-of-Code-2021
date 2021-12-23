ce27010cf296a10dfbd88555ef8cc931553dfe60
Michael Kaufman <michaelkauf@gmail.com>
2021-12-23 12:54:16 -0800

    add base classes

---
+++ file_reader.py
@@ -0,0 +1,13 @@
+class FileReader:
+    def __init__(self, file_name):
+        self._file_name = file_name;
+
+    @property
+    def file_name(self):
+        return self._file_name
+
+    def process_file(self, Class):
+        f = open(self.file_name, 'r')
+        lines = f.readlines()
+        f.close()
+        return [list(Class(int(row), int(col), int(lines[row][col])) for col in range(len(lines[row][:-1]))) for row in range(len(lines))]
---
+++ two_dimensional_array.py
@@ -0,0 +1,91 @@
+from .cell import Cell
+
+class TwoDimensionalArray:
+    def __init__(self, two_dimensional_array): 
+        self._two_dimensional_array = two_dimensional_array
+
+    @property
+    def two_dimensional_array(self):
+        return self._two_dimensional_array
+
+    def row_length(self):
+        return len(self.two_dimensional_array[0])
+
+    def col_length(self):
+        return len(self.two_dimensional_array)
+
+    def get_cell(self, row_idx, col_idx):
+        return self.two_dimensional_array[row_idx][col_idx]
+
+    def flatten(self):
+        return [item for sub_list in self.two_dimensional_array for item in sub_list]
+
+    def is_left_edge(self, loc):
+        return loc.col_idx == 0
+
+    def is_right_edge(self, loc):
+        return loc.col_idx == self.row_length() - 1
+
+    def is_top_edge(self, loc):
+        return loc.row_idx == 0
+
+    def is_bottom_edge(self, loc):
+        return loc.row_idx == self.col_length() - 1
+
+    def get_top(self, loc):
+        return self.two_dimensional_array[loc.row_idx - 1][loc.col_idx]
+
+    def get_bottom(self, loc):
+        return self.two_dimensional_array[loc.row_idx + 1][loc.col_idx]
+
+    def get_right(self, loc):
+        return self.two_dimensional_array[loc.row_idx][loc.col_idx + 1]
+
+    def get_left(self, loc):
+        return self.two_dimensional_array[loc.row_idx][loc.col_idx - 1]
+
+    def get_top_right(self, loc):
+        return self.two_dimensional_array[loc.row_idx - 1][loc.col_idx + 1]
+
+    def get_top_left(self, loc):
+        return self.two_dimensional_array[loc.row_idx - 1][loc.col_idx - 1]
+
+    def get_bottom_right(self, loc):
+        return self.two_dimensional_array[loc.row_idx + 1][loc.col_idx + 1]
+
+    def get_bottom_left(self, loc):
+        return self.two_dimensional_array[loc.row_idx + 1][loc.col_idx - 1]
+
+    # Returns all adjacent locations including diagonally adjacent
+    def get_all_surounding_locs(self, loc):
+        horizontally_and_vertically_adjacent_locs = self.get_adjacent_locs(loc)
+        diagonally_adjacent_locs = []
+
+        if not self.is_top_edge(loc) and not self.is_right_edge(loc):
+            diagonally_adjacent_locs.append(self.get_top_right(loc))
+        if not self.is_top_edge(loc) and not self.is_left_edge(loc):
+            diagonally_adjacent_locs.append(self.get_top_left(loc))
+        if not self.is_bottom_edge(loc) and not self.is_left_edge(loc):
+            diagonally_adjacent_locs.append(self.get_bottom_left(loc))
+        if not self.is_bottom_edge(loc) and not self.is_right_edge(loc):
+            diagonally_adjacent_locs.append(self.get_bottom_right(loc))
+
+        return horizontally_and_vertically_adjacent_locs + diagonally_adjacent_locs
+
+    # Returns all horizontally and veritically adjacent locations
+    def get_adjacent_locs(self, loc):
+        adjacent_locs = []
+        
+        if not self.is_top_edge(loc):
+            adjacent_locs.append(self.get_top(loc))
+        if not self.is_bottom_edge(loc):
+            adjacent_locs.append(self.get_bottom(loc))
+        if not self.is_right_edge(loc):
+            adjacent_locs.append(self.get_right(loc))
+        if not self.is_left_edge(loc):
+            adjacent_locs.append(self.get_left(loc))
+
+        return adjacent_locs
+
+    def __repr__(self) -> str:
+        return str(self.two_dimensional_array)
