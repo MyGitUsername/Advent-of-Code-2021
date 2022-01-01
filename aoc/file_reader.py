c6061100337da1a4f2db72ada480ddedb8a32066
Michael Kaufman <michaelkauf@gmail.com>
2021-12-31 20:26:52 -0800

    make file struct more pythonic

---
+++ aoc/file_reader.py
@@ -0,0 +1,12 @@
+class FileReader:
+    def __init__(self, file_name):
+        self._file_name = file_name;
+
+    @property
+    def file_name(self):
+        return self._file_name
+
+    def process_file(self, Class):
+        with open(self.file_name, 'r') as f:
+            lines = f.readlines()
+        return [list(Class(int(row_idx), int(col_idx), int(char)) for col_idx, char in enumerate(line[:-1])) for row_idx, line in enumerate(lines)]
