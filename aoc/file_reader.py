7d4208896cff7c89e1d6de68d883a67a45a3bb00
Michael Kaufman <michaelkauf@gmail.com>
2022-01-05 22:42:10 -0800

    add new method to file reader class

--- aoc/file_reader.py
+++ aoc/file_reader.py
@@ -1,3 +1,5 @@
+from typing import List, Any
+
 class FileReader:
     def __init__(self, file_name):
         self._file_name = file_name;
@@ -6,7 +8,12 @@ class FileReader:
     def file_name(self):
         return self._file_name
 
-    def process_file(self, Class):
+    def process_file(self, Class) -> List[Any]:
         with open(self.file_name, 'r') as f:
             lines = f.readlines()
         return [list(Class(int(row_idx), int(col_idx), int(char)) for col_idx, char in enumerate(line[:-1])) for row_idx, line in enumerate(lines)]
+
+    def lines(self) -> List[str]:
+        with open(self.file_name, 'r') as f:
+            lines = f.readlines()
+        return [line.strip() for line in lines if line != '\n']
