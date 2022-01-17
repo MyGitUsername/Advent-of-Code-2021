279f15906945a5021b2d761775195f877559667a
Michael Kaufman <michaelkauf@gmail.com>
2022-01-17 10:36:46 -0800

    wip

--- aoc/day14/run.py
+++ aoc/day14/run.py
@@ -2,33 +2,42 @@ from .file_processor import FileProcessor
 import os
 import sys
 from collections import Counter
+from multiprocessing import Pool 
+import os
 import pdb
 
 class Run():
     def __init__(self, polymer_template, rules):
         self.polymer_template = polymer_template
         self.rules = rules
-        
-    def execute_rule(self, pair):
+        self.final_polymer = self.polymer_template
+
+    def execute_rule(self, idx):
+        print('process id:', os.getpid())
+        pair = self.final_polymer[idx] + self.final_polymer[idx + 1]
         try:
             insert = self.rules[pair]
             return insert + pair[1] 
         except:
+            print('Error should not be here!')
+            # pdb.set_trace()
             return pair[1]
-
     
+    # NNCB
     def run(self, steps = sys.maxsize):
-        new_polymer = self.polymer_template
-        for _ in range(0, steps):
-            modified_polymer = new_polymer[0]
-            for j in range(len(new_polymer) - 1):
-                pair = new_polymer[j] + new_polymer[j + 1]
-                modified_polymer += self.execute_rule(pair)
-            new_polymer = modified_polymer
-        return new_polymer
+        for step in range(0, steps):
+            modified_polymer = []
+            with Pool(processes=4) as pool:
+                modified_polymer = pool.map(self.execute_rule, range(len(self.final_polymer) - 1))
+            self.final_polymer =  self.final_polymer[0] + ''.join(modified_polymer) 
+            # pdb.set_trace()
+            print(f'self.final_polymer {self.final_polymer}')
+            print(f'step {step} complete')
 
-    def get_result(self, polymer):
-        counter = Counter(polymer)
+    def get_result(self):
+        if self.final_polymer == self.polymer_template:
+            raise RuntimeError('The run method needs to execute before calling get_results()')
+        counter = Counter(self.final_polymer)
         _, most_common = counter.most_common()[0]
         _, least_common = counter.most_common()[-1]
         return most_common - least_common
@@ -38,6 +47,6 @@ class Run():
 if __name__ == "__main__":
     fp = FileProcessor(os.path.dirname(__file__) + '/input.txt')
     run = Run(fp.polymer_template(), fp.pair_insertion_rules())
-    final_polymer = run.run(40)
-    result = run.get_result(final_polymer)
+    run.run(20)
+    result = run.get_result()
     print(f' res: {result}')
--- aoc/day14/tests/test_run.py
+++ aoc/day14/tests/test_run.py
@@ -10,24 +10,24 @@ class RunTestCase(unittest.TestCase):
         self.run = Run(self.fp.polymer_template(), self.fp.pair_insertion_rules())
 
     def test_run_one_step(self):
-        polymer = self.run.run(1)
-        self.assertEqual(polymer, 'NCNBCHB')
+        pdb.set_trace()
+        self.run.run(1)
+        self.assertEqual(self.run.final_polymer, 'NCNBCHB')
 
     def test_run_two_steps(self):
-        polymer = self.run.run(2)
-        self.assertEqual(polymer, 'NBCCNBBBCBHCB')
+        self.run.run(2)
+        self.assertEqual(self.run.final_polymer, 'NBCCNBBBCBHCB')
 
     def test_run_three_steps(self):
-        polymer = self.run.run(3)
-        self.assertEqual(polymer, 'NBBBCNCCNBBNBNBBCHBHHBCHB')
+        self.run.run(3)
+        self.assertEqual(self.run.final_polymer, 'NBBBCNCCNBBNBNBBCHBHHBCHB')
 
     def test_run_four_steps(self):
-        polymer = self.run.run(4)
-        self.assertEqual(polymer, 'NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB')
+        self.run.run(4)
+        self.assertEqual(self.run.final_polymer, 'NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB')
 
     def test_result(self):
-        polymer = self.run.run(10)
-        pdb.set_trace()
-        res = self.run.get_result(polymer)
+        self.run.run(10)
+        res = self.run.get_result()
         self.assertEqual(res, 1588)
 
